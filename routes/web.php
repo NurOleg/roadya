3ad2372f96e4017295ad86d3584def197e34c5be
NurOleg <oleg.nur94@gmail.com>
2021-04-26 15:53:00 +0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::prefix('admin')->group(function () {
     Route::post('/login', [LoginController::class, 'login'])->name('login');
     Route::get('/register', [LoginController::class, 'registerForm'])->name('register_form');
     Route::post('/register', [LoginController::class, 'register'])->name('register');
+    Route::view('/establishment', 'admin.establishment')->name('establishment');
 
     Route::group(['middleware' => 'auth', 'namespace' => 'admin'], function () {
         Route::group(['prefix' => 'placemark'], function () {
